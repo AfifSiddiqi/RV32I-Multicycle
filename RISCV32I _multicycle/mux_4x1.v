ebaa3ff01b265117c06bf3d401eadbb8ca19b2c4
AfifSiddiqi <152867890+AfifSiddiqi@users.noreply.github.com>
2025-11-23 18:11:33 -0500

    Update mux_4x1.v

--- RISCV32I _multicycle/mux_4x1.v
+++ RISCV32I _multicycle/mux_4x1.v
@@ -1,5 +1,5 @@
-module mux_3x1(
-input[31:0] a,b,c,
+module mux_4x1(
+input[31:0] a,b,c,d,
 input [1:0]s,
 output reg [31:0]q );
 
@@ -9,6 +9,7 @@ begin
 	2'b00: q = a;
 	2'b01: q = b;
 	2'b10: q = c;
+	2'b11: q = d;
 	default: q = 32'bxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx;
 	endcase
 end
